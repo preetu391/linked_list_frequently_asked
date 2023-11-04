10e538d12ff6ba54b044b76e98a7e68e08a9696b
preetu391 <preetu391391@gmail.com>
2023-11-04 12:02:36 +0530

    8

---
+++ Detect-Loop.cpp
@@ -0,0 +1,25 @@
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+// floyd detection algo, slow and fast pointers meet when length of the cycle reached
+bool hasCycle(ListNode *head)
+{
+    ListNode *slow = head, *fast = head;
+    while (fast != nullptr && fast->next != nullptr)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast)
+            return true;
+    }
+    return false;
+}
+void main()
+{
+    return;
+}
